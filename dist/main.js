dde4639d311f9cc0ad9da22c3ad6e6a8379758e6
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-04-11 15:03:38 -0300

    api started ok!

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,16 @@
-console.log('main script ...');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0'
+});
+server.get('/hello', (req, resp, next) => {
+    resp.json({
+        message: 'Ola, deu certo!'
+    });
+    return next();
+});
+server.listen(3000, () => {
+    console.log('API is running on http://localhost:3000');
+});
--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-console.log('main script ...')
+import * as restify from 'restify';
+
+const server = restify.createServer({
+  name: 'meat-api',
+  version: '1.0.0'
+})
+
+server.get('/hello', (req, resp, next) => {
+  resp.json({
+    message: 'Ola, deu certo!'
+  })
+  return next()
+})
+
+server.listen(3000, () => {
+  console.log('API is running on http://localhost:3000')
+})
