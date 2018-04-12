0c392c134440c47c0fbec94032419a7a9cb02abe
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-04-12 16:48:06 -0300

    nao conecta no mlab tbm

--- dist/common/environment.js
+++ dist/common/environment.js
@@ -5,6 +5,6 @@ exports.environment = {
         port: process.env.SERVER_PORT || 3000
     },
     db: {
-        url: process.env.DB_URL || 'mongodb://github:github@meat0-shard-00-00-prsen.mongodb.net:27017,meat0-shard-00-01-prsen.mongodb.net:27017,meat0-shard-00-02-prsen.mongodb.net:27017/test?ssl=true&replicaSet=meat0-shard-0&authSource=admin'
+        url: process.env.DB_URL || 'mongodb://github:github@ds014368.mlab.com:14368/meatdb'
     }
 };
