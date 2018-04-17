330ff9cce014e65b610b8f41b11b842065a5eb5b
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-04-17 12:51:17 -0300

    melhorando tratamento de erro

--- dist/server/error-handler.js
+++ dist/server/error-handler.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleError = (req, resp, err, done) => {
+    console.log(err);
     err.toJSON = () => {
         return {
             message: err.message
@@ -13,6 +14,18 @@ exports.handleError = (req, resp, err, done) => {
             }
             break;
         case 'ValidationError':
+            err.statusCode = 400;
+            const messages = [];
+            for (let name in err.errors) {
+                messages.push({
+                    message: err.errors[name].message
+                });
+            }
+            err.toJSON = () => ({
+                errors: messages
+            });
+            break;
+        case 'ValidatorError':
             err.statusCode = 400;
             break;
     }
--- src/user/user-model.ts
+++ src/user/user-model.ts
@@ -8,15 +8,26 @@ export interface UserInterface extends mongoose.Document {
 
 const userSchema = new mongoose.Schema({
   name: {
-    type: String
+    type: String,
+    required: true,
+    maxlength: 80,
+    minlength: 3
   },
   email: {
     type: String,
+    required: true,
+    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     unique: true
   },
   password: {
     type: String,
+    required: true,
     select: false
+  },
+  gender: {
+    type: String,
+    required: false,
+    enum: ['Male', 'Female']
   }
 })
 
