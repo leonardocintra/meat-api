357c8a8bc6f9826082c0bec0ea05c5f2913ba4e9
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-05-18 13:08:40 -0300

    controle de versao nao funciona ver ajuda

--- dist/user/user-router.js
+++ dist/user/user-router.js
@@ -5,8 +5,25 @@ const model_router_1 = require("../common/model-router");
 class UserRouter extends model_router_1.ModelRouter {
     constructor() {
         super(user_model_1.User);
+        this.findByEmail = (req, resp, next) => {
+            if (req.query.email) {
+                // exemplo de como controlar a versao
+                user_model_1.User.find({
+                    email: req.query.email
+                })
+                    .then(this.renderAll(resp, next))
+                    .catch(next);
+            }
+            else {
+                next();
+            }
+        };
     }
     applyRoutes(application) {
+        // application.get('/users', restify.plugins.conditionalHandler([
+        //   { version: '1.1.3', handler: this.findAll },
+        //   { version: '2.0.0', handler: [this.findByEmail, this.findAll] }
+        // ]));
         application.get('/users', this.findAll);
         application.get('/users/:id', [this.validateId, this.findById]);
         application.post('/users', this.save);
--- src/user/user-router.ts
+++ src/user/user-router.ts
@@ -9,8 +9,27 @@ class UserRouter extends ModelRouter<User> {
     super(User);
   }
 
+  findByEmail = (req, resp, next) => {
+    if (req.query.email) {
+      // exemplo de como controlar a versao
+      User.find({
+        email: req.query.email
+      })
+      .then(this.renderAll(resp, next))
+      .catch(next)
+    } else {
+      next()
+    }
+  }
+
   applyRoutes(application: restify.Server) {
-    application.get('/users', this.findAll)
+
+    // application.get('/users', restify.plugins.conditionalHandler([
+    //   { version: '1.1.3', handler: this.findAll },
+    //   { version: '2.0.0', handler: [this.findByEmail, this.findAll] }
+    // ]));
+
+    application.get('/users', this.findAll)    
     application.get('/users/:id', [this.validateId, this.findById])
     application.post('/users', this.save)
     application.put('/users/:id', [this.validateId, this.replace])
