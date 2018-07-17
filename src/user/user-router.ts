f2ebf8eb23c5ca7cd0dad75e2d9ddc7233e1c643
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-07-17 12:42:21 -0300

    caso quiser por https usar o tutorial

--- src/user/user-router.ts
+++ src/user/user-router.ts
@@ -1,7 +1,7 @@
-import { authorize } from '../security/authz-handler';
-import { authenticate } from '../security/auth-handler';
+import { authorize } from '../security/authz-handler'
+import { authenticate } from '../security/auth-handler'
 import * as restify from 'restify'
-import { User } from "./user-model"
+import { User } from './user-model'
 import { ModelRouter } from '../common/model-router'
 
 class UserRouter extends ModelRouter<User> {
@@ -36,11 +36,11 @@ class UserRouter extends ModelRouter<User> {
       ]}
     ]));
 
-    application.get(`${this.basePath}/:id`, [this.validateId, this.findById])
-    application.post(`${this.basePath}`, this.save)
-    application.put(`${this.basePath}/:id`, [this.validateId, this.replace])
-    application.patch(`${this.basePath}/:id`, [this.validateId, this.update])
-    application.del(`${this.basePath}/:id`, [this.validateId, this.delete])
+    application.get(`${this.basePath}/:id`, [authorize('admin'), this.validateId, this.findById])
+    application.post(`${this.basePath}`, [authorize('admin'), this.save])
+    application.put(`${this.basePath}/:id`, [authorize('admin'), this.validateId, this.replace])
+    application.patch(`${this.basePath}/:id`, [authorize('admin'), this.validateId, this.update])
+    application.del(`${this.basePath}/:id`, [authorize('admin'), this.validateId, this.delete])
     application.post(`${this.basePath}/authenticate`, authenticate)
   }
 }
